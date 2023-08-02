3ad5ed212810c00a25c77c3addaa28c555862e4d
gazsoulriper <gazsoulriper@gmail.com>
2023-08-01 23:14:55 -0300

    Exercícios sobre arrays e loops de repetição.

---
+++ calcular-tabuada.js
@@ -0,0 +1,8 @@
+// Crie um programa que dado um número imprima a sua tabuada.
+
+let numero = 4;
+
+for (let i = 1; i < 11; i++) {
+    const element = numero * i;
+    console.log(numero + ' x ' + i + ' = ' + element);
+}
---
+++ mostrar-somente-numero-par.js
@@ -0,0 +1,11 @@
+// Crie um programa que seja capaz de percorrer um lista de números e imprima
+// cada numero encontrado.
+
+let numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const element = numeros[i];
+    if (element % 2 === 0){
+        console.log(element)
+    }
+}
